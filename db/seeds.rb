b388cbc48fb21c32cf0226b2cd4f5f07f5345099
Steven Brown <sgbrown@mac.com>
2015-03-26 11:34:25 +1300

    purchase link

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ require 'faker'
 
 Product.delete_all
 
-5.times do
-  Product.create(name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph(3), price: rand(99..999999))
+10.times do
+  Product.create(name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph(3), price: rand(9..19999)*10)
 end
